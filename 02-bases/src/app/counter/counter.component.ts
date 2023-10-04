7b2e2d871e4cb00b5a26254bea5f86a7310443de
Jorge Luis Avila Muñoz <jorge.avikla@gmail.com>
2023-10-03 20:39:55 -0600

    avacne

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-counter',
   template: `
-    <h3 h3>Counter: {{counter}}</h3>
+    <h3>Counter: {{counter}}</h3>
     <button (click)="increseBy(+1)">+1</button>
     <button (click)="resetCounter()">Reset</button>
     <button (click)="increseBy(-1)">-1</button>
---
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+   public HeroNames: string[] =['SpiderMan','IronMan','Hukl','Thor'];
+   public deletedHero?:string;
+
+   removeLastHero():void{
+    this.deletedHero = this.HeroNames.pop();
+   }
+}
