cde756187459a78aeb031494805cbab502e1a429
JorgeAvikla <jorge.avikla@gmail.com>
2023-10-10 19:17:26 -0600

    AVACNES

---
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) { }
+
+  get tags () {
+    return this.gifsService.tagHistory;
+  }
+
+  searchTag(busqueda:string) {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
