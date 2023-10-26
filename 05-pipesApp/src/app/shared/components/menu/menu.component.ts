a6c0e62981b6fa508eb504206d926c165c8ca588
Jorge Luis Avila Muñoz <jorge.avikla@gmail.com>
2023-10-25 21:32:48 -0600

    menubar

---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+  // public menuItems: MenuItem[] = [];
+
+  //   ngOnInit() {
+  //       this.menuItems = [
+  //           {
+  //               label: 'Pipes de Angular',
+  //               icon: 'pi pi-desktop',
+  //               items:[
+  //                 {
+  //                   label:'Texto y fechas',
+  //                   icon: 'pi pi-align-left',
+  //                 },
+  //                 {
+  //                   label:'Numeros',
+  //                   icon: 'pi pi-dollar',
+  //                 },
+  //                 {
+  //                   label:'No comunes',
+  //                   icon: 'pi pi-globe',
+  //                 },
+  //               ]
+  //           },
+  //           {
+  //               label: 'Pipes Personalizados',
+  //               icon: 'pi pi-cog',
+  //               items:[
+  //                 {
+  //                   label:'Otro elemento',
+  //                   icon: 'pi pi-cog',
+  //                 }
+  //               ]
+  //           }
+  //       ];
+  //   }
+
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Texto y fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ]
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
+      },
+    ];
+  }
+}
