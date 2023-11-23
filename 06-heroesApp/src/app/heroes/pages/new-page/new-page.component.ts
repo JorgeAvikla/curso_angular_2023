73aa06b1b9e553a6d9e4438f6a2c169a895dbf28
JorgeAvikla <jorge.avikla@gmail.com>
2023-11-23 17:42:25 -0600

    avacne

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,8 +11,31 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+      id:        new FormControl<string>(''),
+      superhero: new FormControl<string>('', {nonNullable: true}),
+      publisher: new FormControl<Publisher>(Publisher.DCComics),
+      alter_ego: new FormControl(''),
+      first_appearance: new FormControl(''),
+      characters: new FormControl(''),
+      alt_img:    new FormControl(''),
+  })
+
   public publishers = [
     {id: 'DC Comics', desc:'DC Comics'},
     {id: 'Marvel Comics', desc:'Marvel Comics'},
   ]
+
+  constructor(private heroesService: HeroesService){}
+
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero
+  }
+
+    onSubmit():void{
+      console.log({formIsValid: this.heroForm.valid,
+        value: this.heroForm.value
+    })
+  }
 }
