d73cde147785cf895e3e6e4f4e68124578a567db
Jorge Luis Avila Muñoz <jorge.avikla@gmail.com>
2023-12-04 06:56:54 -0600

    avacne seccion

--- 07-formApp/src/app/reactive/reactive-routing.module.ts
+++ 07-formApp/src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'basic', component: BasicPageComponent},
+      {path: 'dynamic', component: DynamicComponent},
+      {path: 'switches', component: SwitchesComponent},
+      {path: '**', redirectTo: 'basic'},
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
